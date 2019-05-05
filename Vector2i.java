ef78799fa025550fe027a9bd95b8fd87b1abe5e3
Sven <kuhumwerfer@hotmail.de>
2019-05-06 00:38:57 +0200

    Add files via upload

---
+++ Vector2i.java
@@ -0,0 +1,58 @@
+
+public class Vector2i {
+
+	
+	private int x,y;
+	
+	public Vector2i() {
+		// TODO Auto-generated constructor stub
+	set(0,0);
+	}
+	
+	public Vector2i(Vector2i vector)
+	{
+		set(vector.x,vector.y);
+	}
+	
+
+	public Vector2i(int x, int y)
+	{
+		set(x,y);
+	}
+	
+	public boolean equals(Vector2i lhs, Vector2i rhs)
+	{
+		if(lhs.getX() == rhs.getX() && lhs.getX() == rhs.getY())return true;
+		
+		return false;
+	}
+
+
+	public void set(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public boolean euqals(Object object)
+	{
+		if(!(object instanceof Vector2i))return false;
+		Vector2i vec = (Vector2i) object;
+		if(vec.getX() == this.getX() && vec.getY() == this.getY())return true;
+		return false;
+	}
+	
+	public int getX()
+	{return x;}
+	
+	public int getY()
+	{return y;}
+	
+	public Vector2i getXY()
+	{
+		return new Vector2i(x,y);
+	}
+
+
+}
